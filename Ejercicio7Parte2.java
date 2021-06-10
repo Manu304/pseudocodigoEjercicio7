2f119892c7c37d34866a408b39b87702c823f15d
Manuel Rojas <manuerjs70@gmail.com>
2021-06-10 00:29:16 -0600

    Arregle algoritmo de calculo de porcentajes

--- Ejercicio7Parte2.java
+++ Ejercicio7Parte2.java
@@ -3,21 +3,25 @@
 //y muestre el % de n�meros pares, % de impares y % de ceros generados.
 public class Ejercicio7Parte2 {
     public static void main(String[] args) {
-        int parC = 0, imparC = 0; ceroC = 0;
+        float parC = 0, imparC = 0, ceroC = 0;
         final int CANTIDAD = 15;
+        float pPar, pImpar, pCero;
         for (int i=0; i<CANTIDAD; i++){
             int random = (int) Math.floor(Math.random()*(36)+1);
+            System.out.println("genere: " + random);
             if (random == 0){
-                ceroC++;
+                ceroC += 1.0;
             }else if((random%2) == 0){
-                parC++;
+                parC += 1.0;
             }else{
-                imparC++;
+                imparC += 1.0;
             }
         }
-        double pPar = (parC/CANTIDAD)*100;
-        double pImpar = (imparC/CANTIDAD)*100;
-        double pCero = (ceroC/CANTIDAD)*100;
-    }
-    
+        System.out.println("Contador par: " + parC + ". Contador impar: " + imparC + ". Contador ceros: " + ceroC);
+        
+        pPar = (float) ((parC/CANTIDAD) * 100);
+        pImpar = (float) ((imparC/CANTIDAD) * 100);
+        pCero = (float) (ceroC * (100 / CANTIDAD));
+        System.out.println("De los 15 numeros generados: " + pPar + "% son pares, " + pImpar + "% son impares y " + pCero + "% son ceros.");
+    } 
 }
